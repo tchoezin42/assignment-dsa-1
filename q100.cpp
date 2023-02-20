2fdbf10197e9000aca027150307579c3a30ccf37
tchoezin42 <125869503+tchoezin42@users.noreply.github.com>
2023-02-20 23:07:27 +0530

    Update q100.cpp

--- q100.cpp
+++ q100.cpp
@@ -1,4 +1,4 @@
-#include <iostream>               // another algorithm of question 10
+#include <iostream>               
 
 struct ListNode {
     int val;
@@ -62,3 +62,28 @@ int main() {
 
     return 0;
 }
+
+
+
+/*  question number 10 assignment another algorithm
+ptr1=head1;
+ptr2=head2;
+while(ptr1!=ptr2)
+{
+	if(ptr1==NULL)
+	{
+		ptr=head2;
+	}
+	else
+	ptr=ptr->next;
+	
+	if(ptr2==NULL)
+	{
+		ptr=head1;
+	}
+	else
+	ptr=ptr->next;
+}
+cout<<ptr1->data<< " adress of merging node: "<<ptr1;
+*/
+
